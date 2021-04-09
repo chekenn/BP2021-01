f58f0cee72f1a9f4af4cbf24a53d587000f990c1
Yoojin730 <shongyj2000@naver.com>
2021-04-10 03:35:00 +0900

    Create task05_2019122041.R

---
+++ task05_2019122041.R
@@ -0,0 +1,23 @@
+###https://www.kaggle.com/kimjihoo/coronavirusdataset###
+###위 사이트에서 데이터를 다운로드 받아서 워킹디렉토리 안에 준비하세요.###
+###Download the data from the above site and prepare it in the working directory.###
+
+
+###https://www.kaggle.com/michau96/who-has-covid-19-in-south-korea###
+###위 페이지의 설명과 코드를 참고해서 스크립트를 작성해보고, 데이터 전처리와 분석을 진행하세요.###
+###Refer to the description and code on the page above to write a script, and proceed with data preprocessing and analysis.###
+###참고로 설치가 필요한 패키지가 있는지도 확인하세요.###
+###For reference, check if there are any packages that need to be installed.###
+
+
+###실습해본 스크립트에 주석을 달아 제출하세요.###
+###Please submit a comment on the script you have practiced. ###
+
+
+###단순히 코드를 복붙하는 것이 아니라 실행해보면서 이해하는 과정을 거치길 바랍니다.###
+###Do not simply copy the code, but try to understand it while executing it.###
+
+
+
+
+
